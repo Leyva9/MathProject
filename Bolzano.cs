aba4171da282f10e75a0f92256a54ddded78e0e6
CrashBandicoot <delgadocristhian230@gmail.com>
2023-07-16 02:51:59 -0500

    Comentarios en el codigo, acortamiento en el nombre de una funcion,nuevo metodo Check en la clase Bolzano

--- Bolzano.cs
+++ Bolzano.cs
@@ -1,17 +1,27 @@
 namespace AnalisisProject;
 
-static class Bolzano
+static class Bolzano // Clase que utiliza en diferentes formas las hipotesis para el primer Teorema de Bolzano
 {
+    //Este metodo devuelve un tupla que representa un intervalo cerrado  en el que hay al menos una raiz
     static public (int, int) FindInterval(Polinomio polinomio)
     {
         
         for (int i = 0; ; i++)
         {
-            if(polinomio.EvaluatePol(-Math.Pow(2, i)) * polinomio.EvaluatePol(Math.Pow(2, i)) < 0)
+            if(polinomio.Evaluate(-Math.Pow(2, i)) * polinomio.Evaluate(Math.Pow(2, i)) < 0)
             {
                 (int, int) interval = (-(int)Math.Pow(2, i), (int)Math.Pow(2, i));
                 return interval;
             }
         }
+    }
+    //Permite checar la condicion de bolzano en un intervalo para la existencia de raices
+    static public bool Check((float,float) intervalo,Polinomio polinomio)
+    {
+        if(polinomio.Evaluate(intervalo.Item1)*polinomio.Evaluate(intervalo.Item2)<0)
+        {
+            return true;
+        }
+        return false;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace AnalisisProject;
-internal class Program
+internal class Program 
 {
     private static void Main(string[] args)
     {
-        Polinomio pol = new Polinomio(1, -999999999, 999999, 10000241);
+        Polinomio pol = new Polinomio(1, 5, 8, 3);
         System.Console.WriteLine("a: {0} b: {1}", Bolzano.FindInterval(pol).Item1, Bolzano.FindInterval(pol).Item2);
+        System.Console.WriteLine(Bolzano.Check((-1,1/2),pol)); //Probando la nueva funcion
         
     }
 }
